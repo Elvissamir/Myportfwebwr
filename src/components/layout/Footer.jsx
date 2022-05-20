4e6b5140ee6e78e26cc37c4652a16d8cf6c7fa79
Elvissamir <elvissamir18@gmail.com>
2022-05-20 19:35:20 -0400

    Add Footer and Nav components

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <div className="footer-wrapper">
+            <footer className="footer"></footer>
+        </div>    
+    );
+}
+ 
+export default Footer;
---
+++ src/components/layout/Nav.jsx
@@ -0,0 +1,9 @@
+const Nav = () => {
+    return ( 
+        <div className="nav-wrapper">
+            <nav className="nav"></nav>
+        </div>
+    );
+}
+ 
+export default Nav;
