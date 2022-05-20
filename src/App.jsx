1454269055c6203fd9658f6e52f8a3dffe46979f
Elvissamir <elvissamir18@gmail.com>
2022-05-20 19:32:53 -0400

    Add Routes component. Using Router and Route components

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Router, Route } from "react-router-dom";
+import Routes from "./components/Routes";
+
+const App = () => {
+    return ( 
+        <div className="app-wrapper">
+            <div className="app-nav">
+                
+            </div>
+            <div className="app-content">
+
+            </div>
+            <div className="app-footer">
+                
+            </div>
+        </div>
+    );
+}
+ 
+export default App;
