e30cfcfd52e1c2de8960f942122445fa9e3445e2
Elvissamir <elvissamir18@gmail.com>
2022-05-21 14:45:49 -0400

    Move Nav and Footer components to layout folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import './App.css';
+import Nav from './components/layout/Nav'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <div className="App">
-      <div className='app-nav'></div>
-      <div className='app-content'></div>
-      <div className='app-footer'></div>
+      <div className='app-nav'>
+        <Nav />
+      </div>
+      <div className='app-content'>
+        
+      </div>
+      <div className='app-footer'>
+        <Footer />
+      </div>
     </div>
   );
 }
