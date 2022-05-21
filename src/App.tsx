ce8b7d5110b52cd71efc4899def9cca6b7a8d7f4
Elvissamir <elvissamir18@gmail.com>
2022-05-21 15:54:24 -0400

    Add content to Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Nav from './components/layout/Nav'
 import Footer from './components/layout/Footer'
+import Routes from './components/Routes';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
         <Nav />
       </div>
       <div className='app-content'>
-        
+        <Routes />
       </div>
       <div className='app-footer'>
         <Footer />
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,24 @@
 const Footer = () => {
     return (
         <div className="footer-wrapper">
-            <footer className="footer"></footer>
+            <footer className="footer">
+                <div className="footer-content">
+                    <p>My Social Networks: </p>
+                    <div className="footer-contact">
+                        <div className="footer-email">
+                            <p>elvissam.software@gmail.com</p>
+                        </div>
+                        <div className="footer-networks">
+                            <p>Myportfweb icon</p>
+                            <p>my github icon</p>
+                            <p>My linkedin icon</p>
+                        </div>
+                    </div>
+                    <p>Designed and Developed by Elvis Carrasco</p>
+                    <p>Myportfweb</p>
+                    <p>2022</p>
+                </div>
+            </footer>
         </div>
     )
 }
