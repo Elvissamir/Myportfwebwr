11fc0db528dba828908f013453260238150a398a
Elvissamir <elvissamir18@gmail.com>
2022-05-28 19:56:32 -0400

    Extract sections into Sections component

---
+++ src/components/Sections.tsx
@@ -0,0 +1,29 @@
+const Sections = () => {
+    return ( 
+        <div className='sections-wrapper'>
+            <section className='section'>
+                <p>Start</p>
+            </section>
+            <section className='section'>
+                <p>Skills</p>
+            </section>
+            <section className='section'>
+                <p>Projects</p>
+            </section>
+            <section className='section'>
+                <p>Education</p>
+            </section>
+            <section className='section'>
+                <p>Hire Me</p>
+            </section>
+            <section className='section'>
+                <p>Contact Me</p>
+            </section>
+            <section className='section'>
+                <p>About Me</p>
+            </section>
+        </div> 
+    );
+}
+
+export default Sections;
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,6 +2,7 @@ import Nav from '../components/Nav';
 import MobileMenu from '../components/MobileMenu';
 import useMenus from '../hooks/useMenus';
 import useWindowSize from '../hooks/useWindowSize';
+import Sections from '../components/Sections';
 
 const Home = () => {
     const windowSize = useWindowSize()
@@ -19,29 +20,7 @@ const Home = () => {
                 onToggle={toggleNav} 
                 showingNav={showNav} />
             <main className='main'>
-                <div className='sections-wrapper'>
-                    <section className='section'>
-                        <p>Start</p>
-                    </section>
-                    <section className='section'>
-                        <p>Skills</p>
-                    </section>
-                    <section className='section'>
-                        <p>Projects</p>
-                    </section>
-                    <section className='section'>
-                        <p>Education</p>
-                    </section>
-                    <section className='section'>
-                        <p>Hire Me</p>
-                    </section>
-                    <section className='section'>
-                        <p>Contact Me</p>
-                    </section>
-                    <section className='section'>
-                        <p>About Me</p>
-                    </section>
-                </div>
+                <Sections /> 
             </main>
         </div> 
     );
