c448b724a8da4306f1ac076261de1b63bf3886ef
Elvissamir <elvissamir18@gmail.com>
2022-05-28 19:47:06 -0400

    Add sections

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -18,6 +18,31 @@ const Home = () => {
                 show={showMobile}
                 onToggle={toggleNav} 
                 showingNav={showNav} />
+            <main className='main'>
+                <div className='sections-wrapper'>
+                    <section className='section'>
+                        <p>Start</p>
+                    </section>
+                    <section className='section'>
+                        <p>Skills</p>
+                    </section>
+                    <section className='section'>
+                        <p>Projects</p>
+                    </section>
+                    <section className='section'>
+                        <p>Education</p>
+                    </section>
+                    <section className='section'>
+                        <p>Hire Me</p>
+                    </section>
+                    <section className='section'>
+                        <p>Contact Me</p>
+                    </section>
+                    <section className='section'>
+                        <p>About Me</p>
+                    </section>
+                </div>
+            </main>
         </div> 
     );
 }
