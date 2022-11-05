c018a279fb08827f61c5f6c6c9ced0b7d942112b
Elvis Carrasco <elvissamir18@gmail.com>
2022-11-05 12:36:03 -0400

    Add colors to css styles for BackgroundGrid component

--- src/components/layout/BackgroundGrid.tsx
+++ src/components/layout/BackgroundGrid.tsx
@@ -2,13 +2,13 @@ import { useEffect, useRef, useState } from "react"
 
 const BackgroundGrid = () => {
     const wrapper = useRef<HTMLDivElement | null>(null)
-    const [columns, setColumns] = useState(Math.floor(window.innerWidth / 50))
-    const [rows, setRows] = useState(Math.floor(window.innerHeight / 50))
+    const [columns, setColumns] = useState(Math.floor(window.innerWidth / 100))
+    const [rows, setRows] = useState(Math.floor(window.innerHeight / 100))
     const [tiles, setTiles] = useState<number[]>(new Array(columns * rows).fill(0))
 
     const createGrid = () => {
-        const ncolums = Math.floor(window.innerWidth / 50)
-        const nrows = Math.floor(window.innerHeight / 50)
+        const ncolums = Math.floor(window.innerWidth / 100)
+        const nrows = Math.floor(window.innerHeight / 100)
         const ntiles = new Array(ncolums * nrows).fill(0)
 
         if (wrapper.current) {
