ad61c73f2ebe192871d99f094ded9f953bd07e7c
Elvis Carrasco <elvissamir18@gmail.com>
2022-11-06 09:31:02 -0400

    Add layout structure change for screens >= 1024px (Hide mobilebar and show navbar)

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import useWindowSize from '../../hooks/useWindowSize'
 import BackgroundGrid from "./BackgroundGrid"
 import Content from "./Content"
 import Footer from "./Footer"
@@ -8,6 +9,7 @@ import Navbar from "./Navbar"
 
 const Layout = () => {
     const [ showMobileMenu, setShowMobileMenu ] = useState(false)
+    const { width } = useWindowSize()
 
     const handleToggleMobileMenu = () => {
         setShowMobileMenu(!showMobileMenu)
@@ -17,11 +19,11 @@ const Layout = () => {
         <div className="app-layout">
             <BackgroundGrid />
             <div className="layout-blocks">
-                <Navbar />
-                <MobileMenu showMobileMenu={showMobileMenu} />
-                <Mobilebar 
+                { width >= 1024 && <Navbar />}
+                { width < 1024 && <MobileMenu showMobileMenu={showMobileMenu} />}
+                { width < 1024 && <Mobilebar 
                     showMobileMenu={showMobileMenu}
-                    toggleMobileMenu={handleToggleMobileMenu} />
+                    toggleMobileMenu={handleToggleMobileMenu} />}
                 <Content />
                 <Footer />
             </div>
