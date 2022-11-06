79e124e9718f4011c412c2d29e0c476356a946ce
Elvis Carrasco <elvissamir18@gmail.com>
2022-11-06 17:40:12 -0400

    Add content for ProjectList and ProjectCard components

--- src/components/sections/ProjectCard.tsx
+++ src/components/sections/ProjectCard.tsx
@@ -1,7 +1,28 @@
-const ProjectCard = () => {
+interface ProjectCardProps {
+    title: string 
+    subtitle: string 
+    frontImage: string 
+    githubUrl: string 
+    index: number
+    seeDetails: (index: number) => void
+}
+
+const ProjectCard = ({ title, subtitle, frontImage, githubUrl, index, seeDetails }: ProjectCardProps) => {
     return (
         <div className="project-card-container">
-            <div className="project-card"></div>
+            <div className="project-card">
+                <div className="top">
+                    <img className="project-front-image" src={frontImage} alt="project image" />
+                </div>
+                <div className="project-text-container">
+                    <p className="project-title">{title}</p>
+                    <p className="project-subtitle">{subtitle}</p>
+                </div>
+                <div className="project-btns">
+                    <a className="project-github-link" href={githubUrl}>Github</a>
+                    <button onClick={() => seeDetails(index)} className="project-details-btn">Details</button>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/sections/ProjectList.tsx
+++ src/components/sections/ProjectList.tsx
@@ -6,6 +6,7 @@ interface ProjectData {
     frontImage: string
     images: string[]
     description: string
+    githubUrl: string
 }
 
 const ProjectList = () => {
@@ -15,50 +16,67 @@ const ProjectList = () => {
             subtitle: 'Crypto Portfolio Tracker', 
             frontImage: '', 
             images: [], 
-            description: 'CPT keeps track of your crypto portfolio and markets. The main features are: portfolio management, charts (top cryptos, distribution), crypto markets.'
+            description: 'CPT keeps track of your crypto portfolio and markets. The main features are: portfolio management, charts (top cryptos, distribution), crypto markets.',
+            githubUrl: 'https://github.com/Elvissamir/cryptoPT'
         },
         {
             title: 'Servm', 
             subtitle: 'School Management', 
             frontImage: '', 
             images: [], 
-            description: 'Servm is a management system developed for the "Antonio José" school. It allows management of students, teachers, courses, subjects and more.'
+            description: 'Servm is a management system developed for the "Antonio José" school. It allows management of students, teachers, courses, subjects and more.',
+            githubUrl: 'https://github.com/Elvissamir/registrationManager'
         },
         {
             title: 'Myportfwebwr', 
             subtitle: 'My web portfolio (with React)', 
             frontImage: '', 
             images: [], 
-            description: 'My awesome web portfolio v2. The design was inspired by paperpillars and theta network.'
+            description: 'My awesome web portfolio v2. The design was inspired by paperpillars and theta network.',
+            githubUrl: 'https://github.com/Elvissamir/Myportfweb'
         },
         {
             title: 'AAC', 
             subtitle: 'Algorithms & Challenges', 
             frontImage: '', 
             images: [], 
-            description: 'AAC is a project aimed to gather cool algorithms and coding challenges, and present a UI for them.'
+            description: 'AAC is a project aimed to gather cool algorithms and coding challenges, and present a UI for them.',
+            githubUrl: 'https://github.com/Elvissamir/AAC'
         },
         {
             title: 'Fullrvmovies', 
             subtitle: 'React VMovies', 
             frontImage: '', 
             images: [], 
-            description: 'Movie inventory manager created with Node and React.'
+            description: 'Movie inventory manager created with Node and React.',
+            githubUrl: 'https://github.com/Elvissamir/Fullrvmovies'
         },
         {
             title: 'Front Store', 
             subtitle: 'React/Redux Front Store', 
             frontImage: '', 
             images: [], 
-            description: 'Front Store developed using react and redux made for Scandiweb.'
+            description: 'Front Store developed using react and redux made for Scandiweb.',
+            githubUrl: 'https://github.com/Elvissamir/Scweb'
         }
     ]
+
+    const handleSeeDetails = (index: number) => {
+
+    }
 
     return (
         <div className="project-list-container">
             <div className="project-list">
                 {projectList.map((project, index) => 
-                    <ProjectCard />                
+                    <ProjectCard 
+                        key={index}
+                        index={index}
+                        title={project.title}
+                        subtitle={project.subtitle}
+                        frontImage={project.frontImage}
+                        githubUrl={project.githubUrl} 
+                        seeDetails={handleSeeDetails}/>                
                 )}
             </div>
         </div>
