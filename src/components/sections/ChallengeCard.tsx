052b9bb468a05041a86946efdbf1f5e0afbaf946
Elvis Carrasco <elvissamir18@gmail.com>
2022-11-08 14:26:08 -0400

    Add content and css styles for ChallengesSection

--- src/components/sections/ChallengeCard.tsx
+++ src/components/sections/ChallengeCard.tsx
@@ -1,7 +1,19 @@
-const ChallengeCard = () => {
+import { ChallengeData } from "./ChallengesList"
+
+interface ChallengeCardProps {
+    data: ChallengeData
+}
+
+const ChallengeCard = ({ data }: ChallengeCardProps) => {
     return (
         <div className="challenge-card-container">
-            <div className="challenge-card"></div>
+            <div className="challenge-card">
+                <img className="challenge-image" src={data.imageUrl} alt={data.name} width="350" height='180' />
+                <div className="challenge-info">
+                    <p className="challenge-name">{data.name}</p>
+                    <a className="action-btn" href={data.link}>Visit</a>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/sections/ChallengesList.tsx
+++ src/components/sections/ChallengesList.tsx
@@ -1,7 +1,74 @@
+import ChallengeCard from "./ChallengeCard"
+
+export interface ChallengeData {
+    name: string 
+    imageUrl: string 
+    link: string
+}
+
 const ChallengesList = () => {
+    const imageDir = "/images/algoaac/"
+    const challengeList: ChallengeData[] = [
+        { 
+            name: 'Anagram', 
+            imageUrl: imageDir+'anagram.png',
+            link: 'https://aacsite.netlify.app/anagram'
+        },
+        { 
+            name: 'Binary Tree', 
+            imageUrl: imageDir+'binarytree.png',
+            link: 'https://aacsite.netlify.app/binaryTree'
+        },
+        { 
+            name: 'Linked List', 
+            imageUrl: imageDir+'linkedlist.png',
+            link: 'https://aacsite.netlify.app/linkedList'
+        },  { 
+            name: 'Selection Sort', 
+            imageUrl: imageDir+'selection.png',
+            link: 'https://aacsite.netlify.app/selectionSort'
+        },
+        { 
+            name: 'Shortest Path', 
+            imageUrl: imageDir+'shortest.png',
+            link: 'https://aacsite.netlify.app/shortestPath'
+        },
+        { 
+            name: 'String Reversal', 
+            imageUrl: imageDir+'stringreversal.png',
+            link: 'https://aacsite.netlify.app/stringReversal'
+        },
+        { 
+            name: 'Palindrome', 
+            imageUrl: imageDir+'palindrome.png',
+            link: 'https://aacsite.netlify.app/palindrome'
+        },
+        { 
+            name: 'Fizzbuzz', 
+            imageUrl: imageDir+'fizzbuzz.png',
+            link: 'https://aacsite.netlify.app/fizzbuzz'
+        },
+        { 
+            name: 'Find and Count Vowels', 
+            imageUrl: imageDir+'findvowels.png',
+            link: 'https://aacsite.netlify.app/findvowels'
+        },
+        { 
+            name: 'Max chars', 
+            imageUrl: imageDir+'maxchars.png',
+            link: 'https://aacsite.netlify.app/maxchars'
+        }
+    ]
+
     return (
         <div className="challenges-list-container">
-            <ul className="challenges-list"></ul>
+            <ul className="challenges-list">
+                {challengeList.map((challenge, index) => 
+                    <li className="challenge-item" key={index}>
+                        <ChallengeCard data={challenge} />
+                    </li>
+                )}
+            </ul>
         </div>
     )
 }
