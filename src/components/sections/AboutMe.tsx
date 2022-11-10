02ab760d8c92eb8537637afff06a77955ffe805d
Elvis Carrasco <elvissamir18@gmail.com>
2022-11-10 13:51:20 -0400

    Add content for AboutMe component

--- src/components/sections/AboutMe.tsx
+++ src/components/sections/AboutMe.tsx
@@ -2,7 +2,56 @@ const AboutMe = () => {
     return (
         <div className="about-me-container">
             <div className="about-me">
-                <p></p>
+                <div className="about-text">
+                    <p>Hey, I'm Elvis, a Venezuelan software engineer.!</p>
+                    <p>
+                        I enjoy learning new things, reading, working out, and cooking. 
+                        Listening to music is important for me. My favorite musician is Yanni; I think his compositions are awesome.
+                    </p>
+                    <p>
+                        My favorite books?
+                        <br />
+                        1 - Can't hurt me by David Goggins
+                        2 - The Fastlane Millionaire by MJ DeMarco 
+                    </p>
+                    <p>
+                        My favorite movies? 
+                        - "The Pursuit of Happyness"
+                        - The Harry Potter saga
+                        - The time machine 
+                        - Django Unchained.
+                        - Spiderman (The best spiderman: Tobey Maguire)
+                    </p>
+                    <p>
+                        My favorite series of all time? Cobra Kai
+                    </p>
+                    <p>
+                        Five of my favorite non-instrumental songs? 
+                        1 - Second Wind (You're only human) by Billy Joel 
+                        2 - Don't worry be happy by Bobby Mcferrin 
+                        3 - I can see clearly now by Johnny Nash
+                        4 - Raindrops keep falling on my head by Bj Thomas
+                        5 - The year of the cat by Al Stewart
+                        <br />
+                        Oh man, I could make a long list...
+                    </p>
+                    <p>
+                        Some of my interests, things I'd like to learn and hobbies? 
+                        <br />
+                        1 - Stoicism 
+                        2 - Machine learning 
+                        3 - Leetcode (love it and hate it)
+                        4 - Testing (TDD) 
+                        5 - Playing the guitar
+                    </p>
+                    <p>
+                        Now, what about software? I love the whole experience of software engineering: typing on the keyboard, the colors of the code editor,
+                        learning a new language, creating a system, and then discovering a bug, going to bed thinking about the strategies to solve the problem, 
+                        then the next day finding the solution. The module is finally complete after long hours of effort and testing, the final result... for now.
+                    </p>
+                    <p>Software development has no end. It keeps growing, there's always something to add or improve, and that's definitely what I like the most about it.</p>
+                    <p>Alright, I look forward to hearing from you, talking a little bit, and working together.</p>
+                </div>
             </div>
         </div>
     )
