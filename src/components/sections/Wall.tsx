3be5a0f7059481f3ef4583838b26c4bff8edd165
Elvis Carrasco <elvissamir18@gmail.com>
2022-11-13 15:48:24 -0400

    Add css styles for top of Wall section

--- src/components/sections/Wall.tsx
+++ src/components/sections/Wall.tsx
@@ -1,24 +1,50 @@
+import { ChangeEvent, useState } from "react"
 import ECCircuit from "../ECCircuit"
 
+type Lang = 'eng' | 'spa'
+
 const Wall = () => {
+    const [resume_lang, setResume_lang] = useState<Lang>('eng')
+
+    const handleChangeSelectInput = (e: ChangeEvent<HTMLSelectElement>) => {
+        setResume_lang(e.target.value as Lang)
+    }
+
     return (
         <div id="start" className="wall-container">
             <div className="wall">
                 <div className="top">
                     <div className="wall-text-container">
-                        <p className="ec-elvis-text">ELVIS CARRASCO</p>
+                        <div className="name-container">
+                            <span className="ec-my-name">ELVIS</span>
+                            <span className="ec-my-lname">CARRASCO</span>
+                        </div>
                         <div className="ec-line"></div>
-                        <p className="">Software Engineer</p>
+                        <p className="ec-career">Software Engineer</p>
                     </div>
                     <div className="wall-circuit-container">
                         <div className="wall-circuit">
                             <ECCircuit />
+                            <div className="background-gradient"></div>
                         </div>
                     </div>
                 </div>
                 <div className="bottom">
-                    <p className="wall-bottom-text">My Resume:</p>
-                    <button className="action-btn">Resume</button>
+                    <div className="resume-download-container">
+                        <div className="resume-download-selection">
+                            <p className="wall-bottom-text">My Resume:</p>
+                            <select 
+                                onChange={handleChangeSelectInput}
+                                className="input-select"
+                                name="resume" 
+                                id="resume" 
+                                value={resume_lang}>
+                                    <option className="select-option" value="eng">English</option>
+                                    <option className="select-option" value="spa">Spanish</option>
+                            </select>
+                        </div>
+                        <button className="action-btn">Resume</button>
+                    </div>
                     <div className="mouse"></div>
                 </div>
             </div>
