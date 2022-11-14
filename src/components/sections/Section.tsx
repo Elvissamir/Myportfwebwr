550a76c3e12532c92a867366a440093cf3a32312
Elvis Carrasco <elvissamir18@gmail.com>
2022-11-14 13:31:26 -0400

    Add background to top of Section component

--- src/components/sections/Section.tsx
+++ src/components/sections/Section.tsx
@@ -9,15 +9,17 @@ const Section = ({ id, title, description, content}: SectionProps) => {
     return (
         <div className="section-container">
             <section id={id} className="section">
-                <div className="title-container">
-                    <div className="title-text">
-                        <p className="title">
-                            {title}
-                        </p>
-                        <div className="title-underline"></div>
+                <div className="section-top">
+                    <div className="title-container">
+                        <div className="title-text">
+                            <p className="title">
+                                {title}
+                            </p>
+                            <div className="title-underline"></div>
+                        </div>
                     </div>
+                    { description && <div className="description">{description}</div>}
                 </div>
-                { description && <div className="description">{description}</div>}
                 <div className="content">
                     {content}
                 </div>
