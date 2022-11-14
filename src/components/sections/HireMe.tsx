97e0f86155760f13f6a11eb1d6173c8efe41c1f5
Elvis Carrasco <elvissamir18@gmail.com>
2022-11-14 14:12:40 -0400

    Add link to HireMe section

--- src/components/sections/HireMe.tsx
+++ src/components/sections/HireMe.tsx
@@ -1,9 +1,13 @@
+import sections from "../../core/sections"
+
 const HireMeBanner = () => {
     return (
         <div className="hire-me-container">
             <div className="hire-me">
                 <p className="hire-me-text">Want to hire Me?</p>
-                <button className="action-btn">Let's talk!</button>
+                <a className="action-btn" href={sections.contact.url}>
+                    Let's talk!
+                </a>
             </div>
         </div>
     )
